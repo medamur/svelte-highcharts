c0b3e54f39e11d8aa9d9feff15d87848d47a73b8
medamur <50146323+medamur@users.noreply.github.com>
2024-09-03 12:42:01 +0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import Highcharts from 'highcharts';
+  import Chart from '@highcharts/svelte';
+
+  let series: Highcharts.SeriesLineOptions = {
+    type: 'line',
+    name: 'Jane',
+    data: [1, 0, 4]
+  };
+
+  function addRandomPointToSeries() {
+    series.data?.push(Math.random());
+    series.data = series.data;
+  }
+
+  let options: Highcharts.Options;
+
+  $: options = {
+    title: {
+      text: 'Fruit Consumption'
+    },
+    xAxis: {
+      categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    yAxis: {
+      title: {
+        text: 'Fruit eaten'
+      }
+    },
+    series: [series]
+  };
+</script>
+
+<Chart {options} />
+
+<button on:click={addRandomPointToSeries}>Add random point to series</button>
